968484e0dc6660814cb2e14d50c52b03422fc7e7
Kirk Scheibelhut <kjs@google.com>
2019-01-16 11:01:33 -0800

    Add Aliases and Tier to index exports

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+export {Aliases} from './aliases';
 export {Generation, Generations} from './gen';
 export {ID, toID} from './id';
 export {Nature, Natures} from './natures';
+export {Tier} from './tier';
