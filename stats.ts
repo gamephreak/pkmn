ff0bd371b76979709667ede11eef6ca04d3a7d92
Kirk Scheibelhut <kjs@google.com>
2019-01-16 16:35:57 -0800

    Finish stats, needs tests

--- stats.ts
+++ stats.ts
@@ -1,5 +1,7 @@
 import {extend} from './extend';
+import {CURRENT, Generation} from './gen';
 import {ID} from './id';
+import {Nature} from './natures';
 
 export type Stat = 'hp'|'atk'|'def'|'spa'|'spd'|'spe';
 export type Boost = 'atk'|'def'|'spa'|'spd'|'spe'|'evasion'|'accuracy'|'spc';
@@ -30,6 +32,8 @@ export const SPA: Stat = 'spa';
 export const SPD: Stat = 'spd';
 export const SPE: Stat = 'spe';
 
+const BOOSTS = [1, 1.5, 2, 2.5, 3, 3.5, 4];
+
 const STAT_IDS: {[id: string]: Stat} = {
   'HP': 'hp',
   'hp': 'hp',
@@ -52,40 +56,51 @@ const STAT_IDS: {[id: string]: Stat} = {
   'spe': 'spe',
 };
 
-// TODO redo
-export function display(stat: Stat) {
-  switch (stat) {
-    case HP:
-      return 'HP';
-    case ATK:
-      return 'Atk';
-    case DEF:
-      return 'Def';
-    case SPA:
-      return 'SpA';
-    case SPD:
-      return 'SpD';
-    case SPE:
-      return 'Spe';
-    default:
-      throw new Error('unknown stat ' + stat);
-  }
-}
+const DISPLAY: {[stat: string]: [string, string]} = {
+  hp: ['HP', 'HP'],
+  atk: ['Atk', 'Attack'],
+  def: ['Def', 'Defense'],
+  spa: ['SpA', 'Special Attack'],
+  spd: ['SpD', 'Special Defense'],
+  spe: ['Spe', 'Speed'],
+  spc: ['Spc', 'Special'],
+};
 
 export class Stats {
-  private static fill(p: Partial<StatsTable>, val: number): StatsTable {
-    return extend(true, {}, {
-      'hp': val,
-      'atk': val,
-      'def': val,
-      'spa': val,
-      'spd': val,
-      'spe': val
-    });
+  static calcStat(
+      gen: Generation, stat: Stat, base: number, iv: number, ev: number,
+      level: number, nature?: Nature) {
+    return gen < 3 ? Stats.calcStatRBY(stat, base, iv, ev, level) :
+                     Stats.calcStatADV(stat, base, iv, ev, level, nature);
+  }
+
+  static getStat(s: ID|string): Stat|undefined {
+    return STAT_IDS[s];
+  }
+
+  static fromString(s: string): Stat|undefined {
+    return Stats.getStat(s);
+  }
+
+  static display(str: string, gen: Generation = CURRENT, full = false): string {
+    let s: Stat|'spc'|undefined = STAT_IDS[str];
+    if (!s) {
+      return str;
+    }
+
+    if (gen === 1 && s === SPA) {
+      s = 'spc';
+    }
+    return DISPLAY[s][+full];
   }
 
   static fillIVs(pivs: Partial<StatsTable>): StatsTable {
     return Stats.fill(pivs, 31);
+  }
+
+  static fillEVs(pevs: Partial<StatsTable>, gen: Generation = CURRENT):
+      StatsTable {
+    return Stats.fill(pevs, gen < 3 ? 252 : 0);
   }
 
   static itod(iv: number) {
@@ -94,5 +109,54 @@ export class Stats {
 
   static dtoi(dv: number) {
     return dv * 2 + 1;
+  }
+
+  static boost(b: number): number {
+    if (b > 6) {
+      b = 6;
+    } else if (b < -6) {
+      b = -6;
+    }
+    return BOOSTS[b];
+  }
+
+  static getHPDV(pivs: Partial<StatsTable>): number {
+    const ivs: StatsTable = Stats.fillIVs(pivs);
+    return (Stats.itod(ivs.atk) % 2) * 8 + (Stats.itod(ivs.def) % 2) * 4 +
+        (Stats.itod(ivs.spe) % 2) * 2 + (Stats.itod(ivs.spa) % 2);
+  }
+
+  private static fill(p: Partial<StatsTable>, val: number): StatsTable {
+    return extend(
+        true, {}, {hp: val, atk: val, def: val, spa: val, spd: val, spe: val});
+  }
+
+  private static calcStatRBY(
+      stat: Stat, base: number, iv: number, ev: number, level: number) {
+    // BUG: we ignore EVs - do we care about converting ev to stat experience?
+    const dv = Stats.itod(iv);
+    if (stat === HP) {
+      return Math.floor(((base + dv) * 2 + 63) * level / 100) + level + 10;
+    } else {
+      return Math.floor(((base + dv) * 2 + 63) * level / 100) + 5;
+    }
+  }
+
+  private static calcStatADV(
+      stat: Stat, base: number, iv: number, ev: number, level: number,
+      nature?: Nature) {
+    if (stat === HP) {
+      return base === 1 ?
+          base :
+          Math.floor((base * 2 + iv + Math.floor(ev / 4)) * level / 100) +
+              level + 10;
+    } else {
+      const n: number = !nature ?
+          1 :
+          nature.plus === stat ? 1.1 : nature.minus === stat ? 0.9 : 1;
+      return Math.floor(
+          (Math.floor((base * 2 + iv + Math.floor(ev / 4)) * level / 100) + 5) *
+          n);
+    }
   }
 }
