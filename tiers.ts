6b2d05f3619b86424d4f153d915f4b972f8a019a
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-18 18:32:39 -0800

    Fix Tiers after new introductions

--- tiers.ts
+++ tiers.ts
@@ -1,7 +1,8 @@
+import {extend} from './extend';
 import {ID, toID} from './id';
-export type Tier =  // TODO new tiers..., official vs. unofficial
-    'Uber'|'OU'|'UUBL'|'UU'|'RUBL'|'RU'|'NUBL'|'NU'|'PUBL'|'PU'|'LC'|'AG'|'NFE'|
-    'Unreleased'|'Illegal'|'CAP'|'CAP NFE'|'CAP LC'|'LC Uber';
+
+export type Tier = 'AG'|'Uber'|'OU'|'UUBL'|'UU'|'RUBL'|'RU'|'NUBL'|'NU'|'PUBL'|
+    'PU'|'NFE'|'LC Uber'|'LC'|'Unreleased'|'Illegal'|'CAP'|'CAP NFE'|'CAP LC';
 
 export const UBER: Tier = 'Uber';
 export const OU: Tier = 'OU';
@@ -13,26 +14,64 @@ export const NUBL: Tier = 'NUBL';
 export const NU: Tier = 'NU';
 export const PUBL: Tier = 'PUBL';
 export const PU: Tier = 'PU';
+export const NFE: Tier = 'NFE';
+export const LC_UBER: Tier = 'LC Uber';
 export const LC: Tier = 'LC';
+
+export const AG: Tier = 'AG';
+export const UNRELEASED: Tier = 'Unreleased';
+export const ILLEGAL: Tier = 'Illegal';
+
+export const CAP: Tier = 'CAP';
+export const CAP_NFE: Tier = 'CAP NFE';
+export const CAP_LC: Tier = 'CAP LC';
 
 export const STANDARD: Tier = OU;
 
-const TIERS: Readonly<{[id: string]: Tier}> = {
-  'uber': UBER,
-  'ou': OU,
-  'uubl': UUBL,
-  'uu': UU,
-  'rubl': RUBL,
-  'ru': RU,
-  'nubl': NUBL,
-  'nu': NU,
-  'publ': PUBL,
-  'pu': PU,
-  'lc': LC
+const OFFICIAL: Readonly<{[id: string]: Tier}> = {
+  ou: OU,
+  uu: UU,
+  ru: RU,
+  nu: NU,
+  pu: PU,
+  lc: LC,
 };
+
+const BANLISTS: Readonly<{[id: string]: Tier}> = {
+  uber: UBER,
+  uubl: UUBL,
+  rubl: RUBL,
+  nubl: NUBL,
+  publ: PUBL,
+  lcuber: LC_UBER,
+};
+
+const OTHER: Readonly<{[id: string]: Tier}> = {
+  ag: AG,
+  nfe: NFE,
+  unreleased: UNRELEASED,
+  illegal: ILLEGAL,
+};
+
+const NON_STANDARD: Readonly<{[id: string]: Tier}> = {
+  cap: CAP,
+  capnfe: CAP_NFE,
+  caplc: CAP_LC
+};
+
+const TIERS: Readonly<{[id: string]: Tier}> =
+    extend(true, {}, OFFICIAL, BANLISTS, OTHER, NON_STANDARD);
 
 export class Tiers {
   static fromString(t: string): Tier|undefined {
     return TIERS[toID(t)];
+  }
+
+  static official(t: Tier): boolean {
+    return !!OFFICIAL[t];
+  }
+
+  static isNonstandard(t: Tier): boolean {
+    return !!NON_STANDARD[t];
   }
 }
