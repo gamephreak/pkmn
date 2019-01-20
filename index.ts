6dc18dd6a900517689e329842b5a450afd548262
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-20 12:10:45 -0800

    Add a status type

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 export {Abilities, Ability} from './abilities';
 export {Aliases} from './aliases';
+export {extend} from './extend';
 export {Generation, Generations} from './gen';
 export {ID, toID} from './id';
 export {Move, Moves} from './moves';
@@ -7,6 +8,7 @@ export {Nature, Natures} from './natures';
 export {PokemonSet, Sets} from './sets';
 export {Gender, Pokedex, Species} from './species';
 export {Boost, BoostsTable, Stat, Stats, StatsTable} from './stats';
+export {Status, Statuses} from './status';
 export {Team, Teams} from './teams';
 export {Tier, Tiers} from './tiers';
 export {Type, TypeChart, Types} from './types';
---
+++ status.ts
@@ -0,0 +1,16 @@
+export type Status = 'frz'|'par'|'brn'|'slp'|'psn'|'tox';
+
+const STATUSES: Readonly<{[s in Status]: string}> = {
+  frz: 'Frozen',
+  par: 'Paralyzed',
+  brn: 'Burned',
+  slp: 'Asleep',
+  psn: 'Poisoned',
+  tox: 'Badly Poisoned'
+};
+
+export class Statuses {
+  static toString(s: Status|undefined): string {
+    return s ? STATUSES[s] : 'Healthy';
+  }
+}
