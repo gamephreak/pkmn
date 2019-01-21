b78e8d552bb47be52e03f984739800d725a6d1d0
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-20 18:48:35 -0800

    Fix Wish -> wish side condition

---
+++ side.ts
@@ -0,0 +1,5 @@
+// TODO how do side conditions stack?
+export type SideCondition =
+    'wish'|'auroraveil'|'craftyshield'|'healingwish'|'lightscreen'|'luckychant'|
+    'lunardance'|'matblock'|'mist'|'quickguard'|'reflect'|'safeguard'|'spikes'|
+    'stealthrock'|'stickyweb'|'tailwind'|'toxicspikes'|'wideguard';
