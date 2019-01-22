77f97643e0ea98fe7bfbcd3d8e32603dabf3f7db
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-22 06:35:13 -0800

    Remove battle/field/pokemon/side - belongs in PSC

--- pokemon.ts
+++
@@ -1,8 +0,0 @@
-import {ID} from './id';
-import {Status} from './status';
-// export type VolatileStatus = ID&{__isVolatile: true};
-
-export interface Pokemon {
-  status?: Status;
-  volatiles: {[v: string]: 1};
-}  // class
