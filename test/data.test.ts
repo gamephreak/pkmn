6b45f2c8cbcae3006ec1469803e52d81e568abbd
Kirk Scheibelhut <kjs@scheibo.com>
2019-01-22 18:25:43 -0800

    Add test for Data patch

--- test/data.test.ts
+++ test/data.test.ts
@@ -1,7 +1,9 @@
 import {patch} from '../data';
 
 describe('Data', () => {
-  test.skip('patch', () => {
-    expect(true).toBe(false);  // TODO
+  test('patch', () => {
+    const obj = {'foo': 1, 'bar': 2};
+    const diff = {'foo': {'exists': false}, 'bar': 3};
+    expect(patch(obj, diff)).toEqual({'bar': 3});
   });
 });
