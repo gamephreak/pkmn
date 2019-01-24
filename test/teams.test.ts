31506d08a7d33e654bd70f31c6a2ef90449eecde
Kirk Scheibelhut <kjs@google.com>
2019-01-24 14:48:53 -0800

    Add preliminary test for team

--- test/teams.test.ts
+++ test/teams.test.ts
@@ -20,16 +20,40 @@ function readTeams(
 const GEN7: string = readTeam('gen7ou-bo');
 const GEN4: string = readTeam('gen4ou-stall');
 const GEN1: string = readTeam('gen1ou-cloyster');
-const TEAMS: string = readTeams([
-  {format: 'gen7ou', name: 'SM/BO', team: GEN7},
-  {format: 'gen7ou', name: 'SM/Stall', file: 'gen7ou-stall'},
-  {format: 'gen4ou', name: 'DPP/Stall', team: GEN4},
-  {format: 'gen2ou', name: 'GSC/No Lax', file: 'gen2ou-nolax'}
-]);
+// const TEAMS: string = readTeams([
+//{format: 'gen7ou', name: 'SM/BO', team: GEN7},
+//{format: 'gen7ou', name: 'SM/Stall', file: 'gen7ou-stall'},
+//{format: 'gen4ou', name: 'DPP/Stall', team: GEN4},
+//{format: 'gen2ou', name: 'GSC/No Lax', file: 'gen2ou-nolax'}
+//]);
 
+// TODO
+describe('Team', () => {
+  test.skip('importTeam + exportTeam', () => {
+    expect(Teams.importTeam(GEN7)!.exportTeam()).toEqual(GEN7);
+  });
+
+  test.skip('pack + unpack', () => {});
+  test.skip('toJSON', () => {});
+});
+
+// TODO
 describe('Teams', () => {
-  test.skip('import', () => {
-    const teams = Teams.importTeams(TEAMS);
-    expect(teams.length).toBe(4);
+  test('importTeams + exportTeams', () => {
+    const teams = readTeam('teams');
+    const imported = Teams.importTeams(teams)!;
+    expect(imported.length).toBe(2);
+
+    expect(imported[0].gen()).toBe(7);
+    expect(imported[0].tier()).toBe('OU');
+    expect(imported[0].name).toBe('Rain');
+    expect(imported[0].folder).toBe('');
+
+    expect(imported[1].gen()).toBe(1);
+    expect(imported[1].tier()).toBe('OU');
+    expect(imported[1].name).toBe('Cloyster');
+    expect(imported[1].folder).toBe('RBY');
+
+    expect(Teams.exportTeams(imported)).toEqual(teams);
   });
 });
